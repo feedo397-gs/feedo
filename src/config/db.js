b1df8aa6cb1598fe0aecb10dd1519b0c03ab8f7d
Suraj Hattarakal <suriisuraj8@gmail.com>
2025-11-04 14:24:22 +0530

    Added MongoDB config file

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
-  } catch (err) {
-    console.error("❌ MongoDB connection failed:", err.message);
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
